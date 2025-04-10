82576040bbbbf9fb2cfad6c976232adc3bee2cfa
Felix Bauer <felix.bauer@epfl.ch>
2025-04-10 16:38:32 +0200

    Add data from Rothamsted experiments

--- PARAMETER_PLOT_FILES/prova_Rural_Athens.m
+++ PARAMETER_PLOT_FILES/prova_Rural_Athens.m
@@ -5,23 +5,22 @@
 current_directory = cd;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%
-NN=8760;% 276096;% %%% time Step
+NN=8760; % 166560;% %%% time Step
 %%%%%%%%% Time step 
 dt=3600; %%[s] %%% 
 dth=1; %%[h]
 %%%%%%%%%%%%
 ms=16; %%% Soil Layer 
-cc = 1; %% Crown area
+cc = 2; %% Crown area
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%% METEO INPUT
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-id_location = 'ZURICH_SMA';
-load('..\data\Inputs\Data_Run_Zurich_Fluntern.mat')
-Date=D; clear D 
+id_location = 'Athens';
+load('/data/Inputs/Data_Athens_RUN.mat')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%
 x1=1;
-x2=8760;% 276096;
+x2=8760;% 166560;
 %%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%5
 Date=Date(x1:x2);
@@ -33,12 +32,12 @@ SAB2=SAB2(x1:x2); N=N(x1:x2); Tdew=Tdew(x1:x2);esat=esat(x1:x2);
 PARB=PARB(x1:x2); PARD = PARD(x1:x2);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%
-t_bef= -0.67; t_aft= 1.67;
+t_bef= 1.0; t_aft= 0.0;
 %%%%%%%%%%%%%%%%%%%%
 Ds=esat-ea; %% [Pa] Vapor Pressure Deficit
 Ds(Ds<0)=0;
 %%%%%%%%%%%%%%%%%%%%%%%%%
-load('..\data\Inputs\Ca_Data.mat');
+load('/data/Inputs/Ca_Data.mat');
 d1 = find(abs(Date_CO2-Date(1))<1/36);d2 = find(abs(Date_CO2-Date(end))<1/36);
 Ca=Ca(d1:d2); 
 clear d1 d2 Date_CO2 
@@ -51,16 +50,17 @@ Datam(:,1) = YE; Datam(:,2)= MO; Datam(:,3)= DA; Datam(:,4)= HO;
 clear YE MO DA HO MI SE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %PARAM_IC = strcat(current_directory,'\MOD_PARAM_',id_location);
-PARAM_IC = strcat(current_directory,'\MOD_PARAM_',id_location);
+PARAM_IC = strcat(current_directory,'/PARAMETER_PLOT_FILES/MOD_PARAM_',id_location);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Directory = uigetdir('Window','Insert Directory Noname Package') ;
-Directory='..\T&C_CODE';
+Directory='/code/T&C_CODE';
 cd(Directory)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 MAIN_FRAME ;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-cd(current_directory);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-file_to_save = strcat('results\Ris_',id_location,'.mat');
-save(file_to_save);
+cd(current_directory);
 %%%%%%%%%%%%%%%%%%%%%%%%
+file_to_save = strcat('/results/Ris_',id_location,'.mat');
+save(file_to_save);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
